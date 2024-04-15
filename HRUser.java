625e838644a882593319b03486c64852a124345b
mdreadhossain <162284352+mdreadhossain@users.noreply.github.com>
2024-04-16 01:08:53 +0600

    Add files via upload

---
+++ HRUser.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package event.manager.ms3;
+
+/**
+ *
+ * @author akber
+ */
+class HRUser {
+    
+}
